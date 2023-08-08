6616b02e1cbb395203d65d0419b0ee02f0d58931
Yogesh <iamyogess@gmail.com>
2023-08-08 08:01:38 +0545

    added contact component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Activities from "./components/Activities";
 import Bookings from "./components/Bookings";
 import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Hero />
       <Activities />
       <Bookings />
-      <Gallery/>
+      <Gallery />
+      <Contact />
     </>
   );
 };
